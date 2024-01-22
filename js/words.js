d5a5943a7b4aa2539b4f740b187f778765662347
Neha <nehaverma292002@gmail.com>
2024-01-23 02:14:29 +0530

    File with all the words and hints

---
+++ js/words.js
@@ -0,0 +1,151 @@
+let word = [
+    {
+        word: "addition",
+        hint: "The process of adding numbers"
+    },
+    {
+        word: "meeting",
+        hint: "Event in which people come together"
+    },
+    {
+        word: "number",
+        hint: "Math symbol used for counting"
+    },
+    {
+        word: "exchange",
+        hint: "The act of trading"
+    },
+    {
+        word: "canvas",
+        hint: "Piece of fabric for oil painting"
+    },
+    {
+        word: "garden",
+        hint: "Space for planting flower and plant"
+    },
+    {
+        word: "position",
+        hint: "Location of someone or something"
+    },
+    {
+        word: "feather",
+        hint: "Hair like outer covering of bird"
+    },
+    {
+        word: "comfort",
+        hint: "A pleasant feeling of relaxation"
+    },
+    {
+        word: "tongue",
+        hint: "The muscular organ of mouth"
+    },
+    {
+        word: "expansion",
+        hint: "The process of increase or grow"
+    },
+    {
+        word: "country",
+        hint: "A politically identified region"
+    },
+    {
+        word: "group",
+        hint: "A number of objects or persons"
+    },
+    {
+        word: "taste",
+        hint: "Ability of tongue to detect flavour"
+    },
+    {
+        word: "store",
+        hint: "Large shop where goods are traded"
+    },
+    {
+        word: "field",
+        hint: "Area of land for farming activities"
+    },
+    {
+        word: "friend",
+        hint: "Person other than a family member"
+    },
+    {
+        word: "pocket",
+        hint: "A bag for carrying small items"
+    },
+    {
+        word: "needle",
+        hint: "A thin and sharp metal pin"
+    },
+    {
+        word: "expert",
+        hint: "Person with extensive knowledge"
+    },
+    {
+        word: "statement",
+        hint: "A declaration of something"
+    },
+    {
+        word: "second",
+        hint: "One-sixtieth of a minute"
+    },
+    {
+        word: "library",
+        hint: "Place containing collection of books"
+    },
+    {
+        word: "consider",
+        hint: "deem to be" 
+    },
+    {
+        word: "issue",
+        hint: "some situation or event that is thought about" 
+    },
+    {
+        word: "commit",
+        hint: "perform an act, usually with a negative connotation"
+    },
+    {
+        word: "concern",
+        hint: "something that interests you because it is important"         
+    },
+    {
+        word: "intend",
+        hint: "have in mind as a purpose" 
+    },
+    {
+        word: "practice",
+        hint: "a customary way of operation or behavior"
+    },
+    {
+        word: "evident",
+        hint: "clearly revealed to the mind or the senses or judgment"
+    },
+    {
+        word: "accord",
+        hint: "concurrence of opinion"
+    },
+    {
+        word: "minute",
+        hint: "infinitely or immeasurably small" 
+    },
+    {
+        word: "approach",
+        hint: "move towards" 
+    },
+    {
+        word: "establish",
+        hint: "set up or found" 
+    },
+    
+    {
+        word: "scarce",
+        hint: "deficient in quantity or number compared with the demand" 
+    },
+    {
+        word: "policy",
+        hint: "a plan of action adopted by an individual or social group" 
+    },
+    {
+        word: "straight",
+        hint: "successive, without a break"
+    }
+]
